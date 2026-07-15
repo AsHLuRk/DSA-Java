1c80a850912e4883589c7dce36a484272aa01ed3
Aayush Kumar <146569538+AsHLuRk@users.noreply.github.com>
2026-07-15 19:23:21 +0530

    Time: 4 ms (99.68%) | Memory: 47.9 MB (27.85%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,77 +1,28 @@
 class Solution {
     public boolean validPalindrome(String s) {
-    //     int start = 0;
-    //     int end = s.length()-1;
-    //     boolean available = true;
-
-    //     while(start<=end){
-
-    //         if(s.charAt(start)==s.charAt(end)){
-    //             start++;
-    //             end--;
-    //         }
-    //         else if(s.charAt(start)!=s.charAt(end) && available){
-    //            if(s.charAt(start)==s.charAt(end-1) && s.charAt(start+1)!=s.charAt(end)){
-    //             end--;
-    //             available = false;
-    //            }
-    //            else if(s.charAt(start)!=s.charAt(end-1) && s.charAt(start+1)==s.charAt(end)){
-    //             start++;
-    //             available = false;
-    //            }
-    //            else{
-    //              return palcheck(s.substring(0,start)+s.substring(start+1, s.length())) || palcheck(s.substring(0,end)+s.substring(end+1, s.length())); 
-    //            }
-    //         }
-    //         else{
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-    // public boolean palcheck(String s){
-    //     int start = 0 ;
-    //     int end = s.length()-1;
-    //     while(start<end){
-    //         if(s.charAt(start)!=s.charAt(end)){
-    //             return false;
-    //         }
-    //         if(s.charAt(start)==s.charAt(end)){
-    //             start++;
-    //             end--;
-    //         }
-
-    //     }
-    //     return true;
-      
-      int i =0;
-      int j = s.length()-1;
-      
-
-      while(i<j){
-        if(s.charAt(i)!=s.charAt(j) ){
-            return palcheck(s , i+1 ,j) || palcheck(s,i , j-1);
+        
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return ispalindrome(s , l+1 , r) || ispalindrome(s, l , r-1);
+            }
+            l++;
+            r--;
         }
-       i++;
-       j--;
-      }
-      return true;
 
+        return true;
     }
-     public boolean palcheck(String s, int i , int j){
-   
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+
+    public boolean ispalindrome(String a , int l , int r){
+
+        while(l<r){
+            if(a.charAt(l)!=a.charAt(r)){
                 return false;
             }
-           
-                i++;
-                j--;
-            
+            l++;
+            r--;
         }
         return true;
-
-
     }
-    
 }
