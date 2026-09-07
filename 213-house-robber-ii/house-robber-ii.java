d96716411987876e364b6ef84aa4c09067c3e307
Aayush Kumar <146569538+AsHLuRk@users.noreply.github.com>
2026-09-07 20:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (41.50%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,44 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
-
-
-
-        
         if(nums.length==1){
             return nums[0];
         }
-        if(nums.length==2){
-            return Math.max(nums[0],nums[1]);
+        if(nums.length==0){
+            return 0;
         }
-        
-        return Math.max(robs(nums, 0 , nums.length-2), robs(nums, 1, nums.length-1));
-        
-        // int[] dp = new int[nums.length];
-        // dp[0] = nums[0];
-        // dp[1] = Math.max(nums[0], nums[1]);
-        // for(int i=2; i<nums.length-1; i++){
-        //     dp[i] = Math.max(nums[i]+dp[i-2], 0+dp[i-1]);
-        // }
-        // int ans1 = dp[nums.length-2];
-        // dp[1] = nums[1];
-        // dp[2] = Math.max(nums[1],nums[2]);
-        // for(int i=3; i<nums.length; i++){
-            
-        //   dp[i] = Math.max(nums[i]+dp[i-2], 0+dp[i-1]);
-        // }
-        // int ans2 = dp[nums.length-1];
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        int a= ans(0,nums.length-2, dp , nums);
+        Arrays.fill(dp, -1);
+        int b = ans(1, nums.length-1, dp , nums);
+        return Math.max(a,b);
+    }
 
-        // return Math.max(ans1 , ans2);
+     public int ans(int s,int n , int[] dp, int[] nums){
+
+        if(n==s){
+
+            return nums[s];
+        }
+        if(n==s+1){
+          return Math.max(nums[s], nums[s+1]);
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n] = Math.max(nums[n]+ans(s,n-2,dp,nums), ans(s,n-1,dp,nums));
+        return dp[n];
     }
-  public int robs(int[] nums , int start , int end){
-    int a = nums[start];
-    int b= Math.max(nums[start], nums[start+1]);
-    int ans = 0;
-    for(int i=start+2 ; i<=end; i++){
-         ans = Math.max(a+nums[i], b);
-         a = b;
-         b = ans;
-    }
-    return b;
-  }
 }
