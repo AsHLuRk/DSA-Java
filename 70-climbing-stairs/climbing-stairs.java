89e9f144e554066e734a015c809fc3904cde231c
Aayush Kumar <146569538+AsHLuRk@users.noreply.github.com>
2026-09-07 19:12:40 +0530

    Time: 1 ms (1.17%) | Memory: 41.9 MB (72.81%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,40 +1,19 @@
 class Solution {
+    
     public int climbStairs(int n) {
 
-        int a =1;
-        int b =2;
-
-        int ans =0;
-        for(int i=2; i<=n; i++){
-            ans = a+b;
-            a = b;
-            b = ans;
+        ArrayList<Integer> dp = new ArrayList<>(Collections.nCopies(n+1, -1));
+        return ans(n ,dp);
+    }
+    public int ans(int n, ArrayList<Integer> dp){
+        if(n<=1){
+            return 1;
         }
-        return a;
-    //    if(n<=2){
-    //     return n;
-    //    }
-    //     int[] dp = new int[n+1];
-       
-    //     dp[0] = 1;
-    //     dp[1] = 2;
-
-    //     for(int i=2 ; i<=n; i++){
-    //        dp[i] = dp[i-1]+dp[i-2];
-    //     }
-    //     return dp[n-1];
+        if(dp.get(n)!=-1){
+            return dp.get(n);
+        }
+         dp.add(n , ans(n-1,dp)+ans(n-2,dp));
+         return dp.get(n);
 
     }
-    // public int ways(int n , int[] dp){
-
-    //     if(n<=2){
-    //         dp[n] = n;
-    //         return n;
-    //     }
-    //     if(dp[n]!=0){
-    //         return dp[n];
-    //     }
-    //     dp[n] = ways(n-1, dp)+ways(n-2,dp);
-    //     return dp[n];
-    // }
 }
