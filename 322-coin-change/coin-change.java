239da996f84ea9b72e54d7f7cc75f68a5cd55647
Aayush Kumar <146569538+AsHLuRk@users.noreply.github.com>
2026-09-23 18:58:46 +0530

    Time: 19 ms (40.10%) | Memory: 47 MB (30.88%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,14 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-       int[][] dp = new int[coins.length][amount+1];
-
-       for(int[] arr:dp){
-        for(int i=0; i<arr.length; i++){
-            arr[i] = -1;
+       int[][] dp = new int[coins.length+1][amount+1];
+       dp[0][0] = 0;
+       for(int i=1; i<=amount; i++){
+       dp[0][i] = Integer.MAX_VALUE;
+       }
+       for(int i=1;i<=coins.length; i++){
+        for(int j=1; j<=amount; j++){
+            int pick = Integer.MAX_VALUE;
+            if(coins[i-1]<=j){
+                int sub = dp[i][j-coins[i-1]];
+                if(sub<Integer.MAX_VALUE){
+                    pick = sub+1;
+                }
+            }
+            int nopick = dp[i-1][j];
+            dp[i][j]= Math.min(pick, nopick);
         }
        }
-       int check = ans(coins , amount , coins.length-1, dp);
-       return (check<Integer.MAX_VALUE)?check:-1;
+       return (dp[coins.length][amount])<Integer.MAX_VALUE?dp[coins.length][amount]:-1;
+    //    int check = ans(coins , amount , coins.length-1, dp);
+    //    return (check<Integer.MAX_VALUE)?check:-1;
     }
 
     public int ans(int[] coins , int amount, int n, int[][] dp){
